74b0a0ea85d09e1184508f6be1b395df49cec86f
osawa-koki <github@osawa.dev>
2023-04-12 00:37:13 +0900

    server初期化した。

--- server/app/models/user.rb
+++
@@ -1,7 +0,0 @@
-class User < ApplicationRecord
-  # devise_token_authで必要なモジュールをincludeする
-  include DeviseTokenAuth::Concerns::User
-
-  # deviseで必要なモジュールを指定する
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
-end
