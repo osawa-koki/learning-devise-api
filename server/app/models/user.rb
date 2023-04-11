20547a8c7ccf3ff0bfaf8ec6eb4ed1e76ceeed9d
osawa-koki <github@osawa.dev>
2023-04-12 00:34:52 +0900

    Userモデルの修正。

--- server/app/models/user.rb
+++ server/app/models/user.rb
@@ -1,11 +1,7 @@
 class User < ApplicationRecord
-            # Include default devise modules.
-            devise :database_authenticatable, :registerable,
-                    :recoverable, :rememberable, :trackable, :validatable,
-                    :omniauthable
-            include DeviseTokenAuth::Concerns::User
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  # devise_token_authで必要なモジュールをincludeする
+  include DeviseTokenAuth::Concerns::User
+
+  # deviseで必要なモジュールを指定する
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
 end
