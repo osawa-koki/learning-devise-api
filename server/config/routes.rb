088e4c8aff54c30fb06804a5832964636c820792
osawa-koki <github@osawa.dev>
2023-04-12 22:51:39 +0900

    簡単な認証機能。

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       mount_devise_token_auth_for 'User', at: 'auth'
+
+      resources :users, only: [:index, :show]
     end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
