30d7e234eac522390e1c1c6aa31b8060a981799f
osawa-koki <github@osawa.dev>
2023-04-12 23:01:02 +0900

    ユーザ情報を返す。

--- server/app/controllers/api/v1/users_controller.rb
+++ server/app/controllers/api/v1/users_controller.rb
@@ -1,9 +1,9 @@
 class Api::V1::UsersController < ApplicationController
 
   before_action :authenticate_api_v1_user!
-  
+
   def index
-    @users = User.all
-    render json: @users
+    render json: current_api_v1_user
   end
+
 end
