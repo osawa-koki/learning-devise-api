30bd4f5b9171c14e4c7190bb154ca3dd72f977ad
osawa-koki <github@osawa.dev>
2023-04-12 23:08:07 +0900

    ユーザ情報を変更可能に。

--- server/app/controllers/application_controller.rb
+++ server/app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::API
-        include DeviseTokenAuth::Concerns::SetUserByToken
+  include DeviseTokenAuth::Concerns::SetUserByToken
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :nickname])
+  end
 end
